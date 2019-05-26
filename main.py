f841e45ead79966af98e436df45d34de88605524
Gowtham Behara <gothsiddu@gmail.com>
2019-05-26 17:57:00 +0530

    Parse dataset to get images and labels

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-import cv2
+import cv2, os
+import numpy as np
+from PIL import Image
+
+
+def get_images_and_labels(dataset_path, face_recognizer):
+    images_paths = [os.path.join(dataset_path, f) for f in os.listdir(dataset_path) if
+                    not f.endswith(".sad") and not f.endswith(".txt")]
+    images, labels = [], []
+
+    for image_path in images_paths:
+        image_pil = Image.open(image_path).convert('L')
+        image = np.array(image_pil, 'uint8')
+        label = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
+
+        faces = face_recognizer.detectMultiScale(image)
+        for x, y, w, h in faces:
+            images.append(image[y: y + h, x: x + w])
+            labels.append(label)
+            cv2.imshow("Adding face to training set", image[y: y + h, x: x + w])
+            cv2.waitKey(50)
+
+    return images, labels
+
 
 cascade_path = "venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml"
 face_recognizer = cv2.CascadeClassifier(cascade_path)
+
+dataset_path = 'yalefaces'
+images, labels = get_images_and_labels(dataset_path, face_recognizer)
